278215fe857ed488ae8e408e9ceb0d564caad20d
shimizu-saffle <shimizu.saffle@gmail.com>
2023-11-04 08:04:53 +0900

    feat: Add functionality to list files and directories
     in the current directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, io};
+
+fn main() -> io::Result<()> {
+    let entries = fs::read_dir("./")?;
+
+    for entry in entries {
+        let entry = entry?;
+        print!("{}", entry.path().display())
+    }
+
+    Ok(())
 }
